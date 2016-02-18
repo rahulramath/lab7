386de53870b6b36f05fc31641dca907192404be5
Rahul Ramath <rahulramath1@gmail.com>
2016-02-18 12:52:35 -0800

    analytics lab

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,20 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	var random_num = Math.random();
+	console.log(random_num);
+
+	if(random_num > 0.5) {
+		projects["grid"] = false;
+  		res.render('index', projects);
+  	}
+  	else {
+  		res.redirect('/grid');
+  	}
+};
+
+exports.viewGrid = function(req, res){
+	projects["grid"] = true;
+	console.log(projects.grid);
   	res.render('index', projects);
 };
